50c7aeb337ab339d545600f8258ab60f7e9150da
doe552371-png <doe552371@gmail.com>
2026-09-19 02:34:14 +0300

    chore: remove obsolete remote image config

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "d8j0ntlcm91z4.cloudfront.net",
-      },
-    ],
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
