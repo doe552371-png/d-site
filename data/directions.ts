f7cdf189414d11d22e3cf948ddfb88983a814049
doe552371-png <doe552371@gmail.com>
2026-09-25 09:56:15 +0300

    Add DECOR catalog directions

---
+++ data/directions.ts
@@ -0,0 +1,20 @@
+export const catalogDirections = [
+  {
+    slug: "finishing-materials",
+    name: "Основные материалы",
+    description: "Теневые плинтусы, керамогранит и каменный шпон.",
+    manufacturers: ["laconistiq", "dako", "flatstone"],
+  },
+  {
+    slug: "stucco-decor",
+    name: "Лепной декор",
+    description: "Молдинги, панели, плинтусы и другие декоративные элементы.",
+    manufacturers: ["decomaster", "paolo-arte"],
+  },
+  {
+    slug: "custom-order",
+    name: "Подбор под проект",
+    description: "Подбор и заказ материалов других производителей под проект.",
+    manufacturers: [],
+  },
+];
