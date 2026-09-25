eeec3f798c97baf69b2be8ecd80b913c7dea4a03
doe552371-png <doe552371@gmail.com>
2026-09-25 09:56:02 +0300

    Align manufacturers with catalog structure

--- data/manufacturers.ts
+++ data/manufacturers.ts
@@ -1,26 +1,38 @@
 export const manufacturers = [
   {
-    slug: "decomaster",
-    name: "DECOMASTER",
-  },
-  {
-    slug: "paolo-arte",
-    name: "Paolo Arte",
-  },
-  {
     slug: "laconistiq",
     name: "LACONISTIQ",
-  },
-  {
-    slug: "flatstone",
-    name: "FlatStone",
+    direction: "finishing-materials",
+    productType: "Теневые плинтусы",
   },
   {
     slug: "dako",
     name: "DAKO",
+    direction: "finishing-materials",
+    productType: "Керамогранит",
+  },
+  {
+    slug: "flatstone",
+    name: "FlatStone",
+    direction: "finishing-materials",
+    productType: "Каменный шпон",
+  },
+  {
+    slug: "decomaster",
+    name: "DECOMASTER",
+    direction: "stucco-decor",
+    productType: "Лепной декор",
+  },
+  {
+    slug: "paolo-arte",
+    name: "Paolo Arte",
+    direction: "stucco-decor",
+    productType: "Лепной декор",
   },
   {
     slug: "kudo",
     name: "KUDO",
+    direction: "accessories",
+    productType: "Клей и комплектующие",
   },
 ];
